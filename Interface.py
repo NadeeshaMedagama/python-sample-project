ad2a42ec14eb68c4ab7ef9a6dcab530d2a336f62
Nadeesha <nadeeshamedagama@gmail.com>
2024-07-15 21:28:35 +0530

    Updated

--- Interface.py
+++ Interface.py
@@ -7,4 +7,7 @@ window.title("My First GUI")
 icon = PhotoImage(file = "img.png")
 window.iconphoto(True, icon)
 
+label = Label(window, text = "Hello World", font = ("Arial", 30, 'bold'))
+label.pack()
+
 window.mainloop()
