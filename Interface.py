c4316447947c2757e2cef48fcfce5bf64bc7ca5c
Nadeesha <nadeeshamedagama@gmail.com>
2024-07-15 21:41:15 +0530

    added png

--- Interface.py
+++ Interface.py
@@ -7,7 +7,18 @@ window.title("My First GUI")
 icon = PhotoImage(file = "img.png")
 window.iconphoto(True, icon)
 
-label = Label(window, text = "Hello World", font = ("Arial", 30, 'bold'))
+photo = PhotoImage(file = "img_1.png")
+
+label = Label(window, text = "Hello World",
+              font = ("Arial", 30, 'bold'),
+              fg = "blue",
+              relief = RAISED,
+              bd = 10,
+              padx = 10,
+              pady = 10,
+              image = photo,
+              compound = 'bottom')
+
 label.pack()
 
 window.mainloop()
