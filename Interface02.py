8f841fe2fd1e5d4926cd12570aee86367f9535f4
Nadeesha <nadeeshamedagama@gmail.com>
2024-07-16 19:08:53 +0530

    Interface2.py updated

--- Interface02.py
+++ Interface02.py
@@ -5,15 +5,33 @@ def submit():
     print("You Entered", username)
 
 def delete():
+    name0 = entry.get()
     entry.delete(0, END)
+    print("You Deleted", name0)
+
+def backspace():
+    entry.delete(len(entry.get())-1, END)
 
 window = Tk()
 
+window.geometry('500x500')
+
 entry = Entry(window,
               font= ("arial", 15),
-              width=40)
+              width=40,
+              fg ="blue",
+              bg = "#ccff33")
 
 entry.pack(side = TOP)
+
+entry2 = Entry(window,
+              font= ("arial", 15),
+              width=40,
+              fg ="blue",
+              bg = "#ccff33",
+              show = "*")
+
+entry2.pack(side = TOP)
 
 submit = Button(window,
                 text = "Submit",
@@ -27,9 +45,16 @@ delete = Button(window,
                 text = "Delete",
                 command = delete,
                 width =20,
-                height =2,
-                compound= RIGHT)
+                height =2)
 
 delete.pack()
+
+backspace = Button(window,
+                   text = "Backspace",
+                   command = backspace,
+                   width=20,
+                   height=2)
+
+backspace.pack()
 
 window.mainloop()
